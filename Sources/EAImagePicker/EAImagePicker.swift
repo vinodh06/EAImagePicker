74f0883077c5af4eb20d0b8a5b89017c23c9dcc6
vino <vinodhkumarg@ymail.com>
2023-05-17 03:47:16 +0530

    Initial commit

--- Sources/EAImagePicker/EAImagePicker.swift
+++ Sources/EAImagePicker/EAImagePicker.swift
@@ -1,6 +1,61 @@
-public struct EAImagePicker {
-    public private(set) var text = "Hello, World!"
+//
+//  ImagePicker.swift
+//  BarCodeDetection
+//
+//  Created by vinodh kumar on 17/05/23.
+//
+import SwiftUI
+import UIKit
 
-    public init() {
+public struct EAImagePicker: UIViewControllerRepresentable {
+    @Environment(\.presentationMode) private var presentationMode
+    var sourceType: UIImagePickerController.SourceType = .photoLibrary
+    @Binding var selectedImage: UIImage
+
+    public init(
+        sourceType: UIImagePickerController.SourceType,
+        selectedImage: Binding<UIImage>
+    ) {
+        self.sourceType = sourceType
+        self._selectedImage = selectedImage
+    }
+
+    public func makeUIViewController(
+        context: UIViewControllerRepresentableContext<EAImagePicker>
+    ) -> UIImagePickerController {
+        let imagePicker = UIImagePickerController()
+        imagePicker.allowsEditing = false
+        imagePicker.sourceType = sourceType
+        imagePicker.delegate = context.coordinator
+        return imagePicker
+    }
+
+    public func updateUIViewController(
+        _ uiViewController: UIImagePickerController,
+        context: UIViewControllerRepresentableContext<EAImagePicker>
+    ) {
+    }
+
+    public func makeCoordinator() -> Coordinator {
+        Coordinator(self)
+    }
+
+    final public class Coordinator: NSObject, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+
+        var parent: EAImagePicker
+
+        public init(_ parent: EAImagePicker) {
+            self.parent = parent
+        }
+
+        public func imagePickerController(
+            _ picker: UIImagePickerController,
+            didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey: Any]
+        ) {
+            if let image = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
+                parent.selectedImage = image
+            }
+            parent.presentationMode.wrappedValue.dismiss()
+        }
     }
 }
--- Tests/EAImagePickerTests/EAImagePickerTests.swift
+++ Tests/EAImagePickerTests/EAImagePickerTests.swift
@@ -6,6 +6,6 @@ final class EAImagePickerTests: XCTestCase {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct
         // results.
-        XCTAssertEqual(EAImagePicker().text, "Hello, World!")
+//        XCTAssertEqual(EAImagePicker().text, "Hello, World!")
     }
 }
